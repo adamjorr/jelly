52d1a6e8ae6b84ea11e22d608d5b1b6dc3df0a50
Adam Orr <ajorr1@asu.edu>
2016-08-09 08:42:04 -0700

    change to iterator object

--- jellyin.cc
+++ jellyin.cc
@@ -5,32 +5,43 @@
 #include <iostream>
 #include <memory>
 
-Jellyin::Jellyin(std::string filename) :
-	filename(filename) {
-		fp = gzopen(filename.c_str(),"r");
-		seq = kseq_init(fp);
-	}
-
-Jellyin::~Jellyin(){
-	kseq_destroy(seq);
-	gzclose(fp);
-}
-
-//called repeatedly, each time returning a record from a fastq.
-kseq_t* Jellyin::next(){
-	kseq_read(seq);
-	return seq;
-}
-
 //called once, returning an iterator
 std::unique_ptr<binary_reader> Jellyin::read_hash(std::string fname){
 	std::unique_ptr<std::ifstream> is(new std::ifstream(fname));
+	if (! is->good()){
+		throw std::ios_base::failure("Failure opening " + fname);
+	}
 	jellyfish::file_header header;
 	header.read(*is);
 	std::unique_ptr<binary_reader> reader(new binary_reader(*is,&header));
 	return reader;
 }
 
+//called once, returning an iterator over the fastq
+std::unique_ptr<Jellyit> Jellyin::read_fastq(std::string fname){
+	std::unique_ptr<Jellyit> iterator(new Jellyit(fname));
+	return iterator;
+}
+
+
+Jellyit::Jellyit(std::string filename) :
+	filename(filename) {
+		fp = gzopen(filename.c_str(),"r");
+		if (fp == NULL){
+			throw std::ios_base::failure("Failure opening " + filename);
+		}
+		seq = kseq_init(fp);
+	}
+
+Jellyit::~Jellyit(){
+	kseq_destroy(seq);
+	gzclose(fp);
+}
+
+kseq_t* Jellyit::next(){
+	kseq_read(seq);
+	return seq;
+}
 
 
 
--- jellyin.h
+++ jellyin.h
@@ -7,20 +7,31 @@
 #include <memory>
 #include <jellyfish/jellyfish.hpp>
 
-KSEQ_INIT(gzFile, gzread)
+KSEQ_INIT(gzFile, gzread);
 
-//To read a fastq, create an object and call read_fastq(). EOF = -1
-//To read a hash, simply use the static method read_hash(file).
-class Jellyin{
+//Provides a "next" function to iterate over a fastq file
+//next() returns -1 when EOF is reached.
+class Jellyit{
 	std::string filename;
 	gzFile fp;
 	kseq_t* seq;
 
 	public:
-		Jellyin(std::string filename);
-		~Jellyin();
-		kseq_t* next(); //called repeatedly, each time returning a record from a fastq.
-		static std::unique_ptr<binary_reader> read_hash(std::string fname); //called repeatedly, returning an iterator over the hash
+		Jellyit(std::string filename);
+		~Jellyit();
+		kseq_t* next();
 };
+
+//To read a fastq, use static method read_fastq(file). EOF = -1
+//To read a hash, use the static method read_hash(file).
+class Jellyin{
+	public:
+		static std::unique_ptr<binary_reader> read_hash(std::string fname); //called once, returning an iterator over the hash
+		static std::unique_ptr<Jellyit> read_fastq(std::string fname); //called once, returning an iterator over the fastq
+		static std::unique_ptr<mer_hash> get_hash(std::string fname);//return a hash from FASTQ file(s)
+
+};
+
+
 
 #endif
