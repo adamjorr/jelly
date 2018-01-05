46fe11757e2cfdc11e6a983bcee9d6113d69c3da
Adam Orr <adamjorr@gmail.com>
2018-01-05 16:43:46 -0700

    initial commit of prototype

---
+++ error_kmers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import argparse
+import screed
+import sys
+import khmer
+import pysam
+
+
+
+
+
+
+
+
+def get_erroneous_kmers(graphfile, samfile):
+	htable = khmer.load_countgraph(graphfile)
+	
+
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+    main()
