688353ff9f13f7f7808ffac122b6ad65fb52adf9
Adam Orr <adamjorr@gmail.com>
2018-01-12 15:37:35 -0700

    more prototypes of things

--- error_kmers.py
+++ error_kmers.py
@@ -6,21 +6,22 @@ import khmer
 import pysam
 
 
+def subset_vcf(vcf, conf_regions):
 
+def subset_samfile(samfile, conf_regions):
 
-
-
-def get_erroneous_reads(graphfile,samfile):
+def get_erroneous_reads(graphfile, samfile, vcf):
 	htable = khmer.load_countgraph(graphfile)
+
+def get_kmers_covering(read, pos, ksize):
+	return [read[start:start+ksize] for start in range(0,len(read)-ksize+1) if pos >= start and pos < start+ksize]
 
 def get_erroneous_kmers(graphfile, samfile):
 	ereads = get_erroneous_reads(graphfile,samfile)
-	return [k for kmer in foo(ereads) for k in kmer]
-
-
+	return [kmer for read, pos in ereads for kmer in get_kmers_covering(read, pos, ksize)]
 
 def main():
-	pass
+	print(get_kmers_covering("ATCGAA",3,4))
 
 
 if __name__ == '__main__':
