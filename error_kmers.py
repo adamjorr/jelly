aad20efd9be74dbf69b93f85b33c89d68285240b
Adam Orr <adamjorr@gmail.com>
2018-01-18 14:23:55 -0700

    figure out how i think im going to do this

--- error_kmers.py
+++ error_kmers.py
@@ -45,48 +45,54 @@ def get_ref_base(reffile, chrom, pos):
 def get_kmers_covering(read, pos, ksize):
 	return [read[start:start+ksize] for start in range(0,len(read)-ksize+1) if pos >= start and pos < start+ksize]
 
-def count_from_plp(pileups, refbase, errortable):
+def count_from_plp(pileups, refbase, errortable, countedreads):
+	skippedcounts = 0
 	for read in pileups:
 		if read.is_del or read.is_refskip or read.alignment.query_sequence[read.query_position] != refbase:
-			fastq.write('@{}\n{}\n+\n{}\n'.format(read.query_name, read.query_sequence, read.query_qualities))
-			for k in get_kmers_covering(read.alignment.query_sequence, read.query_position, errortable.ksize()):
-				errortable.count(k) 
-	return errortable, fastq
+			if read.query_name not in countedreads:
+				countedreads.append(read.query_name)
+				# fastq.write('@{}\n{}\n+\n{}\n'.format(read.query_name, read.query_sequence, read.query_qualities))
+				for k in get_kmers_covering(read.alignment.query_sequence, read.query_position, errortable.ksize()):
+					errortable.count(k)
+			else:
+				skippedcounts++
+	print(sys.stderr, "Skipped {} to avoid double counting them.".format(skippedcounts))
+	return errortable, countedreads
 
 #for now ignore sites in the vcf
-#if there are two errors in a kmer, it will be counted twice.
+#if there are two errors in a read, only the first will be counted as an error.
 def count_erroneous_kmers(samfile, ref, conf_regions, vcf, errortable):
 	#try this and see if it works
 	regionstr = ','.join(conf_regions)
-	fastq = io.StringIO()
+	countedreads = set()
+	# fastq = io.StringIO()
 	# for regionstr in get_confident_regions(conf_regions):
 	for col in samfile.pileup(region = regionstr, truncate = True):
 		if col.reference_name in vcf and col.pos in vcf[col.reference_name]:
 				continue
 		else:
 			refbase = get_ref_base(ref, col.reference_name, col.pos)
-			errortable, fastq = count_from_plp(col.pileups, refbase, errortable, fastq)
-	return errortable, fastq
+			errortable, countedreads = count_from_plp(col.pileups, refbase, errortable, countedreads)
+	return errortable
 
 def count_all(samfile, conf_regions, alltable):
 	regionstr = ','.join(conf_regions)
-	fastq = io.StringIO()
+	# fastq = io.StringIO()
 	for read in samfile.fetch(region=regionstr):
 		alltable.consume(read.query_sequence)
-		fastq.write('@{}\n{}\n+\n{}\n'.format(read.query_name, read.query_sequence, read.query_qualities))
-	return alltable, fastq
+		# fastq.write('@{}\n{}\n+\n{}\n'.format(read.query_name, read.query_sequence, read.query_qualities))
+	return alltable
 
 def newinfo(*kwargs):
 	return
 
-def get_abundances(fastq,htable):
-	ksize = htable.ksize()
-	hashsizes = htable.hashsizes()
-	tracking = khmer.Nodegraph(ksize, 1, 1, primes=hashsizes)
-	return htable.abundance_distribution_with_reads_parser(khmer.ReadParser(fastq),tracking)
-
-def print_abundances(abundances):
-	pass
+def get_abundances(samfile, conf_regions, totaltable, errortable):
+	regionstr = ",".join(conf_regions)
+	totalabund, errorabund = [], []
+	for read in samfile.fetch(region=regionstr):
+		totalabund.extend(totaltable.get_kmer_counts(read.query_sequence))
+		errorabund.extend(errortable.get_kmer_counts(read.query_sequence))
+	return totalabund, errorabund
 
 def main():
 	# args = argparse() #TODO
@@ -103,19 +109,26 @@ def main():
 	# print(htable.get("ATC"))
 	# print(htable.get("ATG"))
 
+	#arguments
+	samfilename = ''
+	fafilename = ''
+	bedfilename = ''
+	vcffilename = ''
+
+	#set up hashes
 	khmer.khmer_args.info = newinfo
 	args = khmer.khmer_args.build_counting_args().parse_args()
 	alltable = khmer.khmer_args.create_countgraph(args, ksize=3)
 	errortable = khmer.khmer_args.create_countgraph(args, ksize=3)
 
+	#do things
 	samfile = pysam.AlignmentFile(samfilename)
 	reffile = pysam.FastaFile(fafilename)
 	conf_regions = get_confident_regions(bedfilename)
 	vcf = load_vcf(vcffilename, conf_regions)
-	alltable, allfq = count_all(samfile, conf_regions, alltable)
-	errortable, errorfq = count_erroneous_kmers(samfile, reffile, conf_regions, vcf, errortable)
-	allhashsizes = alltable.hashsizes()
-	errorhashsizes = errortable.hashsizes()
+	alltable = count_all(samfile, conf_regions, alltable)
+	errortable = count_erroneous_kmers(samfile, reffile, conf_regions, vcf, errortable)
+	totalabund, errorabund = get_abundances(samfile, conf_regions, totaltable, errortable)
 
 
 
