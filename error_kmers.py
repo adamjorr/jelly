8ad57c94a502f795404f8c2fe6c520057afee6f6
Adam Orr <adamjorr@gmail.com>
2018-01-18 11:36:51 -0700

    try a thing

--- error_kmers.py
+++ error_kmers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import io
 import screed
 import sys
 import khmer
@@ -7,7 +8,6 @@ import pysam
 import vcf
 import pybedtools
 from pybedtools import BedTool
-
 from khmer import khmer_args
 
 
@@ -48,35 +48,45 @@ def get_kmers_covering(read, pos, ksize):
 def count_from_plp(pileups, refbase, errortable):
 	for read in pileups:
 		if read.is_del or read.is_refskip or read.alignment.query_sequence[read.query_position] != refbase:
-			[errortable.count(k) for k in get_kmers_covering(read.alignment.query_sequence, read.query_position, errortable.ksize())]
-	return errortable
+			fastq.write('@{}\n{}\n+\n{}\n'.format(read.query_name, read.query_sequence, read.query_qualities))
+			for k in get_kmers_covering(read.alignment.query_sequence, read.query_position, errortable.ksize()):
+				errortable.count(k) 
+	return errortable, fastq
 
 #for now ignore sites in the vcf
 #if there are two errors in a kmer, it will be counted twice.
 def count_erroneous_kmers(samfile, ref, conf_regions, vcf, errortable):
 	#try this and see if it works
 	regionstr = ','.join(conf_regions)
+	fastq = io.StringIO()
 	# for regionstr in get_confident_regions(conf_regions):
 	for col in samfile.pileup(region = regionstr, truncate = True):
 		if col.reference_name in vcf and col.pos in vcf[col.reference_name]:
 				continue
 		else:
 			refbase = get_ref_base(ref, col.reference_name, col.pos)
-			errortable = count_from_plp(col.puleups, refbase, errortable)
-	return errortable
+			errortable, fastq = count_from_plp(col.pileups, refbase, errortable, fastq)
+	return errortable, fastq
 
 def count_all(samfile, conf_regions, alltable):
 	regionstr = ','.join(conf_regions)
+	fastq = io.StringIO()
 	for read in samfile.fetch(region=regionstr):
 		alltable.consume(read.query_sequence)
+		fastq.write('@{}\n{}\n+\n{}\n'.format(read.query_name, read.query_sequence, read.query_qualities))
+	return alltable, fastq
 
 def newinfo(*kwargs):
 	return
 
-def print_abundances(outfile, htable):
+def get_abundances(fastq,htable):
 	ksize = htable.ksize()
 	hashsizes = htable.hashsizes()
-	tracking = khmer.Nodegraph()
+	tracking = khmer.Nodegraph(ksize, 1, 1, primes=hashsizes)
+	return htable.abundance_distribution_with_reads_parser(khmer.ReadParser(fastq),tracking)
+
+def print_abundances(abundances):
+	pass
 
 def main():
 	# args = argparse() #TODO
@@ -102,8 +112,8 @@ def main():
 	reffile = pysam.FastaFile(fafilename)
 	conf_regions = get_confident_regions(bedfilename)
 	vcf = load_vcf(vcffilename, conf_regions)
-	alltable = count_all(samfile, conf_regions, alltable)
-	errortable = count_erroneous_kmers(samfile, reffile, conf_regions, vcf, errortable)
+	alltable, allfq = count_all(samfile, conf_regions, alltable)
+	errortable, errorfq = count_erroneous_kmers(samfile, reffile, conf_regions, vcf, errortable)
 	allhashsizes = alltable.hashsizes()
 	errorhashsizes = errortable.hashsizes()
 
